89b8a267765db7a9397f9fa93a2c4a8eba12fe23
damion <540398756@qq.com>
2016-07-08 15:41:34 +0800

    初始化框架搭建

---
+++ FaceDD/DDNavigationController.swift
@@ -0,0 +1,22 @@
+//
+//  DDNavigationController.swift
+//  FaceDD
+//
+//  Created by apple on 16/7/8.
+//  Copyright © 2016年 FaceChina. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DDNavigationController: UINavigationController {
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        self.navigationBar.tintColor = UIColor.redColor()
+    }
+    
+    
+}
---
+++ FaceDD/ME/PersonVC.swift
@@ -0,0 +1,26 @@
+//
+//  PersonVC.swift
+//  FaceDD
+//
+//  Created by apple on 16/7/8.
+//  Copyright © 2016年 FaceChina. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PersonViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationItem.title = "我";
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
